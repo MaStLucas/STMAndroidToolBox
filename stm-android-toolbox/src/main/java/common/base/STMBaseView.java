6755c5ad0fb1c39cd269378cebd3116e83e48090
MaStLucas <821190666@qq.com>
2016-06-06 11:11:37 +0800

    Fix merge conflict.

--- stm-android-toolbox/src/main/java/common/base/STMBaseView.java
+++ stm-android-toolbox/src/main/java/common/base/STMBaseView.java
@@ -9,9 +9,5 @@ public interface STMBaseView<T> {
 
     void setPresenter(T presenter);
 
-<<<<<<< HEAD
     Context getViewContext();
-=======
-    Context getContext();
->>>>>>> 0d81fd23fff796d68593b40245893734b1cf575b
 }
